26dc6257e9a7d8b6049240ec3f805c7cd9b31986
Xaverine POBELLE <xaverinepobelle@orange.fr>
2016-10-18 19:39:13 +0200

    ajout font awesome et debut code footer

--- wp-content/themes/Creation_theme/footer.php
+++ wp-content/themes/Creation_theme/footer.php
@@ -1,9 +1,34 @@
 <div id="footer">
- <p> Copyright &#169; <?php print(date(Y)); ?> <?php bloginfo('name'); ?> <br />
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2">
+				<img id="logo_footer" src="wp-content/themes/Creation_theme/images/logo_footer.png">
+			</div>
+			<div class="col-lg-8">
+				<ul id="menu_footer">
+					<li><a href="#"></a>Home</li>
+					<li><a href="#"></a>DIY</li>
+					<li><a href="#"></a>Beauté</li>
+					<li><a href="#"></a>Contact</li>
+					<li><a href="#"></a>Mentions légales</li>
+				</ul>
+			</div>
+			<div class="col-lg-2">
+				<div class="rs_footer">
+					<a class="btn btn-block btn-social btn-twitter">
+					    <span class="fa fa-twitter"></span> Sign in with Twitter
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+ <!-- <p> Copyright &#169; <?php print(date(Y)); ?> <?php bloginfo('name'); ?> <br />
   Blog propulsé par 
   <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par 
   <a href="http://www.fran6art.com">Fran6art</a> <br /> 
   <a href="feed:<?php bloginfo('rss2_url'); ?>">Articles (RSS)</a> et 
   <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Commentaires (RSS)</a>. 
-  <?php echo get_num_queries(); ?> requêtes. <?php timer_stop(1); ?> secondes. </p>
+  <?php echo get_num_queries(); ?> requêtes. <?php timer_stop(1); ?> secondes. </p> -->
 </div>
