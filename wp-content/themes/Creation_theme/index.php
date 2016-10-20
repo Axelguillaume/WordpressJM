1fadbabec42710179ea63eb141c9857bcbc3b8d1
Unknown <guilllaumeaxel@gmail.com>
2016-10-20 18:51:44 +0200

    Ajout séparation page d'accueil

--- wp-content/themes/Creation_theme/index.php
+++ wp-content/themes/Creation_theme/index.php
@@ -1,5 +1,5 @@
 	<?php get_header(); ?> <!-- ouvrir header,php --> 
-	<div id="content"> 
+	<!-- <div id="content"> 
 		<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?> 
 			<div class="post" id="post-<?php the_ID(); ?>"> 
 				<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2> 
@@ -8,7 +8,53 @@
 			</div> 
 		<?php endwhile; ?> 
 		<?php endif; ?> 
+	</div> -->
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="text-center">
+					<h1>Derniers articles DIY</h1>
+					<ul>
+						<?php
+						    $recentPosts = new WP_Query();
+						    $recentPosts->query('showposts=5');
+						?>
+						<?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+						    <li>
+						    	<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+						    </li>
+						<?php endwhile; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
+	<div class="container-fluid separation">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="col-md-4">
+					<div class="text-center">
+						<img src="wp-content/themes/Creation_theme/images/1230.png" alt="" class="img-responsive">
+						<h2>Vidéo Réalisées</h1>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="text-center">
+						<img src="wp-content/themes/Creation_theme/images/120.png" alt="" class="img-responsive">
+						<h2>Paires de Chaussures</h1>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="text-center">
+						<img src="wp-content/themes/Creation_theme/images/50000.png" alt="" class="img-responsive">
+						<h2>Followers</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<?php get_sidebar(); ?>
 	<?php get_footer(); ?>
 	</div>
