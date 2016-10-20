48eab9de837e5f8d2f5e5a9911f9bcee13aa9854
Unknown <guilllaumeaxel@gmail.com>
2016-10-20 15:45:37 +0200

    Ajout Slider

--- wp-content/themes/Creation_theme/header.php
+++ wp-content/themes/Creation_theme/header.php
@@ -20,38 +20,96 @@
 
 </head> 
 <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="page">
+                    <div id="header"> 
+                        <nav class="navbar navbar-default navbar-fixed-top">
+                          <div class="container">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                              </button>
+                              <a class="navbar-brand" href="#"><img src="wp-content/themes/Creation_theme/images/logo_EXE.png" alt="" /></a>
+                            </div>
+                            <form class="navbar-form navbar-right">
+                                <button type="submit" class="btn"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                                <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                                </div>
+                            </form>
 
-    <div id="page">
-        <div id="header"> 
-
-            <nav class="navbar navbar-default navbar-fixed-top">
-              <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="#"><img src="wp-content/themes/Creation_theme/images/logo_EXE.png" alt="" /></a>
-                </div>
-                <form class="navbar-form navbar-right">
-                    <button type="submit" class="btn"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                    <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav navbar-left">
+                                <li><a href="#header">Accueil</a></li>
+                                <li><a href="#skill">Qui sommes nous?</a></li>
+                                <li><a href="#equipe">DIY</a></li>
+                                <li><a href="#contact">Contact</a></li>
+                              </ul>
+                            </div><!-- /.navbar-collapse -->
+                          </div><!-- /.container-fluid -->
+                        </nav>
                     </div>
-                </form>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav navbar-left">
-                    <li><a href="#header">Accueil</a></li>
-                    <li><a href="#skill">Qui sommes nous?</a></li>
-                    <li><a href="#equipe">DIY</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                  </ul>
-                </div><!-- /.navbar-collapse -->
-              </div><!-- /.container-fluid -->
-            </nav>
+                </div>
+            </div>
         </div>
+    </div>
+    
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="slider" class="carousel slide" data-ride="carousel">
+                      <!-- Indicators -->
+                      <ol class="carousel-indicators">
+                        <li data-target="#slider" data-slide-to="0" class="active"></li>
+                        <li data-target="#slider" data-slide-to="1"></li>
+                        <li data-target="#slider" data-slide-to="2"></li>
+                      </ol>
+
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                          <img src="wp-content/themes/Creation_theme/images/chaussure.jpg" alt="...">
+                          <div class="carousel-caption">
+                            <h1>MICHELLE</h1>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="wp-content/themes/Creation_theme/images/chaussure.jpg" alt="...">
+                          <div class="carousel-caption">
+                            <h1>Jacqueline</h1>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="wp-content/themes/Creation_theme/images/chaussure.jpg" alt="...">
+                          <div class="carousel-caption">
+                            <h1>Jacqueline</h1>
+                          </div>
+                        </div>
+                      </div>
+
+                      <!-- Controls -->
+                      <a class="left carousel-control" href="#slider" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="right carousel-control" href="#slider" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="wp-content/themes/Creation_theme/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+        <script src="wp-content/themes/Creation_theme/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+
