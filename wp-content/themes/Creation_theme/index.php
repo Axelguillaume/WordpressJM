0cefef85e76d5f2e871eb58ba2b3e5bca04a6053
Unknown <guilllaumeaxel@gmail.com>
2016-10-21 18:09:51 +0200

    Fix Slider index

--- wp-content/themes/Creation_theme/index.php
+++ wp-content/themes/Creation_theme/index.php
@@ -11,7 +11,52 @@
 	</div> -->
 	
 	
+	<div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="slider" class="carousel slide" data-ride="carousel">
+                      <!-- Indicators -->
+                      <ol class="carousel-indicators">
+                        <li data-target="#slider" data-slide-to="0" class="active"></li>
+                        <li data-target="#slider" data-slide-to="1"></li>
+                        <li data-target="#slider" data-slide-to="2"></li>
+                      </ol>
 
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                          <img src="<?php bloginfo('url'); ?>/wp-content/themes/Creation_theme/images/slider1.jpg" alt="...">
+                          <div class="carousel-caption">
+                            <h1>Jacqueline & Michelle</h1>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="<?php bloginfo('url'); ?>/wp-content/themes/Creation_theme/images/slider2.jpg" alt="...">
+                          <div class="carousel-caption">
+                            <h1>Jacqueline & Michelle</h1>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="<?php bloginfo('url'); ?>/wp-content/themes/Creation_theme/images/slider3.jpg" alt="...">
+                          <div class="carousel-caption">
+                            <h1>Jacqueline & Michelle</h1>
+                          </div>
+                        </div>
+                      </div>
+
+                      <!-- Controls -->
+                      <a class="left carousel-control" href="#slider" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="right carousel-control" href="#slider" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                </div>
+            </div>
+        </div>
 	<div class="carou">
 		<div class="container-fluid">
 			<div class="row">
